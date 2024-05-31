a6eefa72b83eb8ce17c3a1fd3f2eb9dee5724956
Nurmuhamad <Nurmuhamad1201f@gmail.com>
2024-05-31 17:19:17 +0500

    ubdated_dark_mode

--- src/pages/help/Help.jsx
+++ src/pages/help/Help.jsx
@@ -5,159 +5,159 @@ const Help = () => {
     const {t} = useTranslation()
   return (
     <div className=' px-[100px]'>
-        <section className="bg-blue-500 w-[90%] m-auto mt-[50px] rounded-[20px] dark:bg-black text-white p-4">
+        <section className="bg-blue-500 dark:text-white w-[90%] m-auto mt-[50px] rounded-[20px] dark:bg-black text-white p-4">
   <h1 className="text-[50px] py-[20px] font-bold mb-2">{t("salom70")}</h1>
   <p className="text-lg">{t("salom48")}</p>
 </section>
 
 
 <section>
-    <input className=' w-[90%] m-auto border-b-2 ' type="text" placeholder='Введите свой вопрос...' />
+    <input className=' w-[90%] dark:text-white m-auto border-b-2 ' type="text" placeholder='Введите свой вопрос...' />
 </section>
 
 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom71")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom71")}</h1>
   </div>
   <div className="bg-white p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom72")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom72")}</h1>
   </div>
   <div className="  p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom73")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom73")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom74")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom74")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom75")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom75")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom76")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom76")}</h1>
   </div>
 </section>
 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
   <div className=" p-4 rounded-lg">
-    <h1 className=" text-[40px]">{t("salom78")}</h1>
+    <h1 className=" dark:text-white text-[40px]">{t("salom78")}</h1>
   </div>
   <div className="bg-white p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom12")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom12")}</h1>
   </div>
   <div className="  p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom70")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom70")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom50")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom50")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom15")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom15")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom76")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom76")}</h1>
   </div>
 </section>
 
 
 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-    <h1 className=' font-semibold text-[30px] ' >{t("salom79")}</h1>
+    <h1 className=' font-semibold dark:text-white text-[30px] ' >{t("salom79")}</h1>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold ">{t("salom78")}</h1>
+    <h1 className="text-xl dark:text-white font-bold ">{t("salom78")}</h1>
   </div>
   <div className="bg-white p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom12")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom12")}</h1>
   </div>
   <div className="  p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom20")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom20")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom10")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom10")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom15")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom15")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom16")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom16")}</h1>
   </div>
 </section>
 
 
 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-    <h1 className=' font-semibold text-[30px] ' >{t("salom80")}</h1>
+    <h1 className=' font-semibold dark:text-white text-[30px] ' >{t("salom80")}</h1>
  
   
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom50")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom50")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom60")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom60")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom70")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom70")}</h1>
   </div>
 </section>
 
 
 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-    <h1 className=' font-semibold text-[30px] ' >{t("salom81")}</h1>
+    <h1 className=' font-semibold dark:text-white text-[30px] ' >{t("salom81")}</h1>
  
   
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom53")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom53")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom45")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom45")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom55")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom55")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom5")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom5")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom30")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom30")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom40")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom40")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom80")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom80")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom70")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom70")}</h1>
   </div>
 </section>
 
 <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4">
-    <h1 className=' font-semibold text-[30px] ' >{t("salom82")}</h1>
+    <h1 className=' font-semibold dark:text-white text-[30px] ' >{t("salom82")}</h1>
  
   
-  <div className="p-4 rounded-lg">
+  <div className="p-4 rounded-lg dark:text-white ">
     <h1 className="text-xl font-bold">{t("salom30")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom20")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom20")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom10")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom10")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom11")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom11")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom30")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom30")}</h1>
   </div>
   <div className="p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom25")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom25")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom60")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom60")}</h1>
   </div>
   <div className=" p-4 rounded-lg">
-    <h1 className="text-xl font-bold">{t("salom80")}</h1>
+    <h1 className="text-xl dark:text-white font-bold">{t("salom80")}</h1>
   </div>
 </section>
 
 <section className="bg-blue-500 w-[90%] m-auto mt-[50px] rounded-[20px] dark:bg-black text-white p-4">
-  <h1 className="text-[50px] py-[20px] font-bold mb-2">{t("salom69")}</h1>
+  <h1 className="text-[50px] dark:text-white py-[20px] font-bold mb-2">{t("salom69")}</h1>
  
 </section>
 
